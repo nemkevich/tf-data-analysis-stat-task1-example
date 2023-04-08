bcbecb96fc6a612f17d6d8ce0f5befa7223e358b
Maxim Nemkevich <nemkevichma@gmail.com>
2023-04-08 02:35:39 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,7 @@ import numpy as np
 chat_id = 82953459 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    avg_shipments = np.mean(x)
-    L = avg_shipments / 2
-    return L
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    return (np.mean(x)/2)
